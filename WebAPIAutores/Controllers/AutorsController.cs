2879b0090b0161127479f31d5f0a0051c49c0b1e
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2022-12-16 12:12:38 -0600

    Add project files.

---
+++ WebAPIAutores/Controllers/AutorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAutores.Entities;
+
+namespace WebAPIAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() { };
+        }
+    }
+}
