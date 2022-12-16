943e2d636e2f6a9783f8811151006eae0a568ea3
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2022-12-16 15:39:03 -0600

    Autors Controller

--- WebAPIAutores/Controllers/AutorsController.cs
+++ WebAPIAutores/Controllers/AutorsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPIAutores.Entities;
 
 namespace WebAPIAutores.Controllers
@@ -7,10 +8,61 @@ namespace WebAPIAutores.Controllers
     [ApiController]
     public class AutorsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Autor>> Get()
+        private readonly ApplicationDBContext context;
+        public AutorsController(ApplicationDBContext context)
         {
-            return new List<Autor>() { };
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autors.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if (autor.Id != id)
+            {
+                return BadRequest("The Id doesn't match with id url");
+            }
+
+            var autorExisted = await context.Autors.AnyAsync(x => x.Id == id);
+            if (!autorExisted)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var autorExisted = await context.Autors.AnyAsync(x => x.Id == id);
+            if (!autorExisted)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor { Id = id });
+            
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
