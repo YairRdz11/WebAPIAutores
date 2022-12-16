375ad46b629a17c6c291835cac4381b0b6965029
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2022-12-16 15:54:51 -0600

    Book Controller

---
+++ WebAPIAutores/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entities;
+
+namespace WebAPIAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public BooksController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Book>> Get(int id)
+        {
+            return await context.Books.Include(x => x.Autor).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Book book)
+        {
+            var autorExisted = await context.Autors.AnyAsync(x => x.Id == book.AutorId);
+            if (!autorExisted)
+            {
+                return BadRequest($"Autor id not found of: {book.AutorId}");
+            }
+
+            context.Add(book);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
--- WebAPIAutores/Startup.cs
+++ WebAPIAutores/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace WebAPIAutores
 {
@@ -14,7 +15,8 @@ namespace WebAPIAutores
         {
             // Add services to the container.
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
             services.AddDbContext<ApplicationDBContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("defaultConnection"))
