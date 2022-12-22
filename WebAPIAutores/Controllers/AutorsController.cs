43c6a1eca4e8d344bef1810adb07db1137053e20
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2022-12-22 15:22:39 -0600

    Get Autor by id

--- WebAPIAutores/Controllers/AutorsController.cs
+++ WebAPIAutores/Controllers/AutorsController.cs
@@ -25,6 +25,19 @@ namespace WebAPIAutores.Controllers
             return await context.Autors.ToListAsync();
         }
 
+        [HttpGet("{id:int")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autors.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(autor is null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
         [HttpPost]
         public async Task<ActionResult> Post(AutorCreationDTO autorCreationDTO)
         {
