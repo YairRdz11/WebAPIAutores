09b938b827ad4ef6e345cae97c6eec08e06ec7a9
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2022-12-23 16:06:39 -0600

    Comment migration

--- WebAPIAutores/ApplicationDBContext.cs
+++ WebAPIAutores/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 using WebAPIAutores.Entities;
 
 namespace WebAPIAutores
@@ -7,6 +8,7 @@ namespace WebAPIAutores
     {
         public DbSet<Autor> Autors { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
         public ApplicationDBContext(DbContextOptions options) : base(options)
         {
         }
--- WebAPIAutores/Entities/Book.cs
+++ WebAPIAutores/Entities/Book.cs
@@ -6,8 +6,10 @@ namespace WebAPIAutores.Entities
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         [CamelCaseValidation]
         [StringLength(maximumLength: 250)]
         public string Title { get; set; }
+        public List<Comment> Comments { get; set; }
     }
 }
